775834d20d14ec7eb90a8fd2119ff69df9839f97
Parth shah <parthmshah21@gmail.com>
2023-09-12 16:35:18 +0530

    feat: master keywords details

--- src/Components/Accordian/Accordian.jsx
+++ src/Components/Accordian/Accordian.jsx
@@ -23,137 +23,151 @@ function AccordionComponent({
     }
   };
 
-  useEffect(() => {
-    setActiveKey(
-      isShowAllKeywords > 0 ? accordianData.map((_, index) => index) : []
-    );
-  }, [isShowAllKeywords]);
+    const getMasterKeywordsDataFunction = async () => {
+      const urlKeywordsDetails = await axios.get(
+        `${BASEURL}/masterKeywordsDetails`
+      );
+      if (urlKeywordsDetails.status === 200) {
+        setUrlKeywordsData(urlKeywordsDetails.data);
+      } else {
+        setUrlKeywordsData([]);
+      }
+    };
 
-  useEffect(() => {
-    setActiveKey(accordianData.map((_, index) => index));
-    setEditArray(accordianData.map((_, index) => index));
-  }, [isEditAllToggle]);
+    useEffect(() => {
+      setActiveKey(
+        isShowAllKeywords > 0 ? accordianData.map((_, index) => index) : []
+      );
+    }, [isShowAllKeywords]);
 
-  useEffect(() => {
-    setActiveKey([]);
-    setEditArray([]);
-  }, [isSaveAllToggle]);
+    useEffect(() => {
+      setActiveKey(accordianData.map((_, index) => index));
+      setEditArray(accordianData.map((_, index) => index));
+    }, [isEditAllToggle]);
 
-  useEffect(() => {
-    urlKeywordsDataFunction();
-  }, []);
+    useEffect(() => {
+      setActiveKey([]);
+      setEditArray([]);
+    }, [isSaveAllToggle]);
 
-  const openAccordionFunction = (index) => {
-    const elemIndex = activeKey.indexOf(index);
-    if (elemIndex > -1) {
-      activeKey.splice(elemIndex, 1);
-      setActiveKey([...activeKey]);
-    } else {
-      setActiveKey([...activeKey, index]);
-    }
-  };
-  const editButtonFunction = (index) => {
-    setEditArray([...editArray, index]);
-  };
-  const onSaveButtonFunction = (index) => {
-    const elemIndex = editArray.indexOf(index);
-    if (elemIndex > -1) editArray.splice(elemIndex, 1);
-    setEditArray([...editArray]);
-  };
+    useEffect(() => {
+      urlKeywordsDataFunction();
+    }, []);
 
-  return (
-    <Accordion className="m-3" activeKey={activeKey} alwaysOpen>
-      {Object.keys(urlKeywordsData).map((urlItem, index) => {
-        return (
-          <Accordion.Item eventKey={index} key={index}>
-            <Accordion.Header onClick={() => openAccordionFunction(index)}>
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <div className="d-flex align-items-center">
-                  <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      id={`inlineCheckbox1${index}`}
-                      value={index}
-                      checked={urlKeywordsData[urlItem]?.isChecked}
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                    />
-                  </div>
-                  <div className="mt-1">{urlItem}</div>
-                </div>
-                {editArray.includes(index) ? (
-                  <div>
-                    <Button
-                      variant="outline-success"
-                      className="ms-2"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        onSaveButtonFunction(index);
-                      }}
-                    >
-                      Save
-                    </Button>
-                    <Button
-                      variant="outline-danger"
-                      className="ms-2 me-4"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        onSaveButtonFunction(index);
-                      }}
-                    >
-                      Delete
-                    </Button>
-                  </div>
-                ) : (
-                  <Button
-                    variant="outline-primary"
-                    className="ms-2 me-4"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      editButtonFunction(index);
-                    }}
-                  >
-                    Edit
-                  </Button>
-                )}
-              </div>
-            </Accordion.Header>
-            <Accordion.Body>
-              {Object.keys(urlKeywordsData[urlItem]?.keywords).map(
-                (keywordItem, keyWordIndex) => {
-                  return (
-                    <div
-                      class="form-check form-check-inline"
-                      key={keyWordIndex}
-                    >
+    const openAccordionFunction = (index) => {
+      const elemIndex = activeKey.indexOf(index);
+      if (elemIndex > -1) {
+        activeKey.splice(elemIndex, 1);
+        setActiveKey([...activeKey]);
+      } else {
+        setActiveKey([...activeKey, index]);
+      }
+    };
+    const editButtonFunction = (index) => {
+      setEditArray([...editArray, index]);
+    };
+    const onSaveButtonFunction = (index) => {
+      const elemIndex = editArray.indexOf(index);
+      if (elemIndex > -1) editArray.splice(elemIndex, 1);
+      setEditArray([...editArray]);
+    };
+
+    return (
+      <Accordion className="m-3" activeKey={activeKey} alwaysOpen>
+        {Object.keys(urlKeywordsData).map((urlItem, index) => {
+          return (
+            <Accordion.Item eventKey={index} key={index}>
+              <Accordion.Header onClick={() => openAccordionFunction(index)}>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                  <div className="d-flex align-items-center">
+                    <div className="form-check form-check-inline">
                       <input
-                        class="form-check-input"
+                        className="form-check-input"
                         type="checkbox"
-                        id={`inlineCheckbox1${keyWordIndex}`}
-                        value={keyWordIndex}
-                        disabled={!editArray.includes(index)}
-                        defaultChecked={
-                          urlKeywordsData[urlItem]?.keywords[keywordItem]
-                        }
+                        id={`inlineCheckbox1${index}`}
+                        value={index}
+                        defaultChecked={urlKeywordsData[urlItem]?.isChecked}
                         onClick={(e) => {
                           e.stopPropagation();
                         }}
                       />
-                      <label class="form-check-label" for="inlineCheckbox1">
-                        {keywordItem}
-                      </label>
                     </div>
-                  );
-                }
-              )}
-            </Accordion.Body>
-          </Accordion.Item>
-        );
-      })}
-    </Accordion>
-  );
+                    <div className="mt-1">{urlItem}</div>
+                  </div>
+                  {editArray.includes(index) ? (
+                    <div>
+                      <Button
+                        variant="outline-success"
+                        className="ms-2"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onSaveButtonFunction(index);
+                        }}
+                      >
+                        Save
+                      </Button>
+                      <Button
+                        variant="outline-danger"
+                        className="ms-2 me-4"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onSaveButtonFunction(index);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  ) : (
+                    <Button
+                      variant="outline-primary"
+                      className="ms-2 me-4"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        editButtonFunction(index);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  )}
+                </div>
+              </Accordion.Header>
+              <Accordion.Body>
+                {Object.keys(urlKeywordsData[urlItem]?.keywords).map(
+                  (keywordItem, keyWordIndex) => {
+                    return (
+                      <div
+                        className="form-check form-check-inline"
+                        key={keyWordIndex}
+                      >
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          id={`inlineCheckbox1${keyWordIndex}`}
+                          value={keyWordIndex}
+                          disabled={!editArray.includes(index)}
+                          defaultChecked={
+                            urlKeywordsData[urlItem]?.keywords[keywordItem]
+                          }
+                          onClick={(e) => {
+                            e.stopPropagation();
+                          }}
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="inlineCheckbox1"
+                        >
+                          {keywordItem}
+                        </label>
+                      </div>
+                    );
+                  }
+                )}
+              </Accordion.Body>
+            </Accordion.Item>
+          );
+        })}
+      </Accordion>
+    );
 }
 
 export default AccordionComponent;
