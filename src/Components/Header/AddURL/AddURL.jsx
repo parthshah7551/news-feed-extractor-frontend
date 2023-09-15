eb1b4f28b4bd636019f38bef32b5112703a86349
Parth shah <parthmshah21@gmail.com>
2023-09-15 17:20:24 +0530

    feat: add utl api support

--- src/Components/Header/AddURL/AddURL.jsx
+++ src/Components/Header/AddURL/AddURL.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { BASEURL } from "../../../Constants/constant";
+import axios from "axios";
 
 function AddURL() {
+  const [inputURLText, setInputURLText] = useState("");
+  const addURLFunction = async () => {
+    console.log("inputURLText: ", inputURLText);
+    await axios.post(`${BASEURL}/addURL`, { url: inputURLText });
+    setInputURLText("");
+  };
   return (
     <form className="form-inline d-flex align-items-center flex-wrap m-2">
       <div className="form-group">
@@ -9,9 +17,19 @@ function AddURL() {
           className="form-control"
           id="addURLtext"
           placeholder="Enter the URL"
+          value={inputURLText}
+          onChange={(e) => setInputURLText(e.target.value.trim())}
         />
       </div>
-      <button type="submit" className="ms-2 btn btn-primary">
+      <button
+        type="submit"
+        className="ms-2 btn btn-primary"
+        onClick={async (e) => {
+          e.preventDefault();
+          await addURLFunction();
+        }}
+        disabled={!inputURLText}
+      >
         Add URL
       </button>
     </form>
